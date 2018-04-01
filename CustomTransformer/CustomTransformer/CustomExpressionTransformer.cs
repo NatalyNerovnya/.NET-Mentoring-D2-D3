1300757929e3c060aef5885f1b502422c9686a4e
Nataly Nerovnya <natanerovnja@gmail.com>
2018-04-01 20:43:44 +0300

    Fix VisitLambda

--- CustomTransformer/CustomTransformer/CustomExpressionTransformer.cs
+++ CustomTransformer/CustomTransformer/CustomExpressionTransformer.cs
@@ -25,6 +25,5 @@ namespace CustomTransformer
 
             return expression;
         }
-
     }
 }
---
+++ CustomTransformer/CustomTransformer/VariableSubstitution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CustomTransformer
+{
+    public class VariableSubstitution<T>: ExpressionVisitor
+    {
+        public Dictionary<string, T> ValuesDictionary { get; set; }
+
+        public VariableSubstitution(Dictionary<string, T> valuesDictionary)
+        {
+            ValuesDictionary = valuesDictionary;
+        }
+
+        protected override Expression VisitLambda<TU>(Expression<TU> expression)
+        {
+            if (expression.NodeType == ExpressionType.Parameter)
+            {
+                VisitParameter((ParameterExpression)expression.Body);
+            }
+            
+            return Expression.Lambda(Visit(expression.Body), expression.Parameters);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return ValuesDictionary.ContainsKey(node.Name) ? Expression.Constant(ValuesDictionary[node.Name]) : base.VisitParameter(node);
+        }
+
+
+    }
+}
