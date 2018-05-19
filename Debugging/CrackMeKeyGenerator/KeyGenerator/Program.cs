5d7106936ef822b6db3f0dd2e20e8c14eeef07dd
Nataly Nerovnya <natanerovnja@gmail.com>
2018-05-19 13:51:15 +0300

    Add debug task

---
+++ Debugging/CrackMeKeyGenerator/KeyGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace KeyGenerator
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        public static void Main(string[] args)
+        {
+            var key = GenerateKey();
+
+            Clipboard.SetText(key);
+
+            Console.WriteLine(key);
+            Console.WriteLine("This key is copied to clickboard");
+            Console.ReadLine();
+
+        }
+        public static string GenerateKey()
+        {
+            NetworkInterface networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(); ;
+            byte[] addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes(); ;
+            DateTime dateTime = DateTime.Now.Date;
+
+            var dateBytes = BitConverter.GetBytes(dateTime.ToBinary());
+            var source = addressBytes
+                .Select((b, index) => b ^ dateBytes[index])
+                .Select(x => x <= 999 ? x * 10 : x)
+                .ToArray();
+
+            return string.Join("-", source);
+        }
+    }
+}
